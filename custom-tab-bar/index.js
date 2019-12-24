b3a6c3f4156b9ed2ab526894441168be19e37d9d
hyk <770860791@qq.com>
2019-12-24 11:50:28 +0800

    修改成型自定义TabBar

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,42 +1,59 @@
 Component({
-     data: {
-          selected: 0,
-          color: "#7A7E83",
-          selectedColor: "#3cc51f",
-          iconPath: "/image/qr_scan.png",
-          selectedIconPath: "/image/icon_component_HL.png",
-          list: [{
-               pagePath: "/index/index",
-               iconPath: "/image/icon_component.png",
-               selectedIconPath: "/image/icon_component_HL.png",
-               text: "组件"
-          }, {
-               pagePath: "/index/index2",
-               iconPath: "/image/icon_API.png",
-               selectedIconPath: "/image/icon_API_HL.png",
-               text: "接口"
-          }]
-     },
-     attached() {},
-     methods: {
-          switchTab(e) {
-               const data = e.currentTarget.dataset
-               const url = data.path
-               wx.switchTab({
-                    url
-               })
-               this.setData({
-                    selected: data.index
-               })
-               console.log(data.index)
-          },
-          openScan(e) {
-               wx.scanCode({
-                    scanType: ['barCode', 'qrCode'],
-                    success(res) {
-                         console.log(res)
-                    }
-               })
-          }
-     }
+      data: {
+            selected: 0,
+            color: "#7A7E83",
+            selectedColor: "#3cc51f",
+            iconPath: "/image/bell.png",
+            selectedIconPath: "/image/icon_component_HL.png",
+            list: [{
+                  pagePath: "/index/index",
+                  iconPath: "/image/home.png",
+                  selectedIconPath: "/image/home-active.png",
+                  text: "主页"
+            }, {
+                  pagePath: "/equ/index",
+                        iconPath: "/image/medical-box.png",
+                        selectedIconPath: "/image/medical-box-active.png",
+                  text: "设备"
+            }, 
+                  {
+                        pagePath: "/equ/index",
+                        iconPath: "/image/bell.png",
+                        selectedIconPath: "/image/bell.png",
+                        text: " "
+                  },{
+                  pagePath: "/order/index",
+                        iconPath: "/image/resourc.png",
+                        selectedIconPath: "/image/resource-active.png",
+                  text: "order"
+            }, {
+                  pagePath: "/person/index",
+                        iconPath: "/image/person.png",
+                        selectedIconPath: "/image/person-active.png",
+                  text: "我"
+            }]
+      },
+      attached() {},
+      methods: {
+            switchTab(e) {
+                  const data = e.currentTarget.dataset
+                  const url = data.path
+                  console.log(url)
+                  wx.switchTab({
+                        url: url
+                  })
+                  this.setData({
+                        selected: data.index
+                  })
+                  console.log(data.index)
+            },
+            openScan(e) {
+                  wx.scanCode({
+                        scanType: ['barCode', 'qrCode'],
+                        success(res) {
+                              console.log(res)
+                        }
+                  })
+            }
+      }
 })
